8af092524ad56ba72295828fbf6d911abd0ed476
Shashank Shekhar <vshekhas@amazon.com>
2022-12-03 13:49:47 -0800

    Adding session-controller

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
 import express from 'express';
 import cors from 'cors';
+import session from 'express-session'
 import HelloController from "./controllers/hello-controller.js";
 import UsersController from "./controllers/users-controller.js";
+import SessionController from "./controllers/session-controller.js";
+
 import mongoose from "mongoose";
 
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+    autoIndex: false,
+    maxPoolSize: 10,
+    socketTimeoutMS: 45000,
+    family: 4
+}
 const CONNECTION_STRING = 'mongodb+srv://anime-webdev:p0mAHNcE4K44uPNU@cluster0.4v1rsn2.mongodb.net/?retryWrites=true&w=majority'
-mongoose.connect(CONNECTION_STRING);
+mongoose.connect(CONNECTION_STRING, options);
 
 const app = express()
-app.use(cors())
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3000'
+}))
+
+app.use(session({
+    secret: 'secretKEYabc',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}))
 
 app.use(express.json());
 HelloController(app);
 UsersController(app);
-
+SessionController(app);
 app.listen(4000);
--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,63 +1,83 @@
 import * as userDao from '../daos/UserDao.js'
-import {findByCredentials, findByUsername} from "../daos/UserDao.js";
 
-const findUser = async (req, res) => {
-    const user = await userDao.findUser(req.params.usid);
-    res.json(user);
-}
+let currentUser = null
 
-const createUser = async (req, res) => {
-    const newUser = req.body;
-    const insertedUser = await userDao.createUser(newUser);
-    res.json(insertedUser);
-}
-
-const findAllUsers = async (req, res) => {
-    const user = await userDao.findAllUsers();
-    res.json(user);
-}
-
-const deleteUser = async (req, res) => {
-    const usid = req.params.usid
-    const status = await userDao.deleteUser(usid)
-    res.json(status);
-}
-
-const updateUser = async (req, res) => {
-    const usid = req.params.usid
-    const updates = req.body
-    const status = await userDao.updateUser(usid, updates)
-    res.json(status);
-}
-
-const registerUser = async (req, res) => {
-    const user = req.body
-    const existingUser = await findByUsername(user.username)
-    if (existingUser) {
-        res.sendStatus(403)
-        return
+const UsersController = (app) => {
+    const findUser = async (req, res) => {
+        const user = await userDao.findUser(req.params.usid);
+        res.json(user);
     }
-    const insertedUser = await userDao.createUser(user)
-    res.json(insertedUser)
-}
 
-const login = async (req, res) => {
-    const credentials = req.body
-    const existingUser = await findByCredentials(credentials.username, credentials.password)
-    if (!existingUser) {
-        res.sendStatus(403)
-        return
+    // const createUser = async (req, res) => {
+    //     const newUser = req.body;
+    //     const insertedUser = await userDao.createUser(newUser);
+    //     res.json(insertedUser);
+    // }
+
+    const findAllUsers = async (req, res) => {
+        const user = await userDao.findAllUsers();
+        res.json(user);
     }
-    res.json(existingUser)
-}
 
-export default(app) => {
+    const deleteUser = async (req, res) => {
+        const usid = req.params.usid
+        const status = await userDao.deleteUser(usid)
+        res.json(status);
+    }
+
+    const updateUser = async (req, res) => {
+        const usid = req.params.usid
+        const updates = req.body
+        const status = await userDao.updateUser(usid, updates)
+        res.json(status);
+    }
+
+    const registerUser = async (req, res) => {
+        const user = req.body
+        const existingUser = await userDao.findByUsername(user.username)
+        if (existingUser) {
+            res.sendStatus(403)
+            return
+        }
+        const insertedUser = await userDao.createUser(user)
+        req.session['currentUser'] = insertedUser
+        res.json(insertedUser)
+    }
+
+    const login = async (req, res) => {
+        const credentials = req.body
+        const existingUser = await userDao
+            .findByCredentials
+                (credentials.username, credentials.password)
+        if (!existingUser) {
+            res.sendStatus(401)
+            return
+        }
+        req.session['currentUser'] = existingUser
+        res.json(existingUser)
+    }
+
+    const logout = (req, res) => {
+        req.session.destroy()
+        res.sendStatus(200)
+    }
+
+    const profile = (req, res) => {
+        if (req.session['currentUser']) {
+            res.send(req.session['currentUser'])
+        } else {
+            res.sendStatus(403)
+        }
+    }
+
     app.get('/profile/all', findAllUsers);
     app.get('/profile/:usid', findUser);
-    app.post('/profile', createUser)
+    app.get('/profile', profile)
     app.delete('/profile/:usid', deleteUser)
     app.put('/profile/:usid', updateUser)
     app.post('/register', registerUser)
     app.post('/login', login)
-    app.post('/logout', () => {})
+    app.post('/logout', logout)
 }
+
+export default UsersController
