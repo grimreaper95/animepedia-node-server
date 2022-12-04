6d967f22d275fe984483165821e1f3b1c44aa4a5
nishamishra12 <nishamishra093@gmail.com>
2022-12-04 18:30:07 -0500

    Anime review: schema and model

---
+++ mongoose/reviews/ReviewSchema.js
@@ -0,0 +1,10 @@
+import mongoose, {Schema} from 'mongoose';
+
+const reviewSchema = mongoose.Schema({
+                                       animeId: Number,
+                                       reviewBy:  {type: Schema.Types.ObjectId, ref: "UserModel"},
+                                       review: String,
+                                       rating: Number,
+                                   }, {collection: "reviews"});
+
+export default ReviewScehma;
