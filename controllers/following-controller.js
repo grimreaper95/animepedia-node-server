4b557df03fc86567795ffc3124aaa2b9289a41f8
kanishkasoni <soni.kan@northeastern.edu>
2022-12-06 15:17:52 -0500

    unfollow

--- controllers/following-controller.js
+++ controllers/following-controller.js
@@ -1,4 +1,5 @@
 import * as followingDao from '../daos/FollowingDao.js'
+import {compileETag} from "express/lib/utils.js";
 
 const addFollower = async (req, res) => {
     const following = req.body;
@@ -11,9 +12,19 @@ const findAllFollowers = async (req, res) => {
     const followers = await followingDao.findAllFollowers(userId);
     res.json(followers)
 }
-export default(app) => {
+
+const unfollow = async (req, res) => {
+    console.log(req.params)
+    const unfollowId = req.params;
+    const status = await followingDao.deleteFollower(unfollowId);
+    res.json(status)
+
+}
+
+export default (app) => {
     app.get('/follow/:usid', findAllFollowers);
     app.post('/follow', addFollower);
+    app.delete('/unfollow/:usid/:fid', unfollow)
 
 }
 
