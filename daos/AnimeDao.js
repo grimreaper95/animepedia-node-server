a3545de42c291c84f82cac6cdc75e1c85cbea4e5
nishamishra12 <nishamishra093@gmail.com>
2022-12-06 19:40:35 -0500

    Java doc for anime id

--- daos/AnimeDao.js
+++ daos/AnimeDao.js
@@ -1,5 +1,6 @@
 import animeModel from "../mongoose/anime/AnimeModel.js";
 
+/** animeId is the id by which an anime is represented in the Jikan API **/
 export const findAnime = (aid) => animeModel.findOne({animeId: aid});
 
 export const createAnime = (anime) => animeModel.create(anime);
