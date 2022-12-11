7ed1504ed4a67298ab4af3a712bf29650f0062b1
nishamishra12 <nishamishra093@gmail.com>
2022-12-11 10:17:05 -0500

    Added anime detail to review

--- mongoose/reviews/ReviewSchema.js
+++ mongoose/reviews/ReviewSchema.js
@@ -1,10 +1,11 @@
 import mongoose, {Schema} from 'mongoose';
 
 const reviewSchema = mongoose.Schema({
-                                       animeId: Number,
-                                       reviewBy:  {type: Schema.Types.ObjectId, ref: "UserModel"},
-                                       review: String,
-                                       rating: Number,
-                                   }, {collection: "reviews"});
-
+                                         animeId: Number,
+                                         reviewBy: {type: Schema.Types.ObjectId, ref: "UserModel"},
+                                         review: String,
+                                         rating: Number,
+                                         animeImage: String,
+                                         animeTitle: String
+                                     }, {collection: "reviews"});
 export default reviewSchema;
