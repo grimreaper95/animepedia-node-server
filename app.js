8d938e9a2a4479e920eee1e3f5d38b9e86d9684e
Shashank Shekhar <vshekhas@amazon.com>
2022-12-11 18:44:09 -0800

    Removing allow-origin * from app.js

--- app.js
+++ app.js
@@ -34,13 +34,13 @@ app.use(cors({
 }))
 
 
-//
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// //
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Credentials", true);
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 
 app.use(session({
